340c48f80803d3a5b7356d5e0003c6cd6706af9b
Your Name <you@example.com>
2022-03-11 20:40:48 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -12,8 +12,10 @@ y en c almacena el valor de la suma de a más b.
 */
 
 void suma(int a, int b, int * c) {
-    
-
+   scanf("%d", a);
+   scanf("%d", b);
+   c = a+b;
+}
 /*
 Esta función suma los N primeros números del arreglo a
 */
